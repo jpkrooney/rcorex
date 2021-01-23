a56c1ae46667127d59d577ad75c7866bd64ce56e
jpkrooney <jrooney@rcsi.ie>
2021-01-23 15:36:27 +0100

    logo update

--- generate_logo.R
+++ generate_logo.R
@@ -10,7 +10,7 @@ df <- data.frame(df)
 # Build a hierarchical corex
 set.seed(12345)
 L1 <- biocorex(df, 10, 2, marginal_description = "gaussian", repeats = 1)
-L2 <- biocorex(L1$labels, 6, 2, marginal_description = "discrete", repeats = 1)
+L2 <- biocorex(L1$labels, 4, 2, marginal_description = "discrete", repeats = 1)
 L3 <- biocorex(L2$labels, 1, 2, marginal_description = "discrete", repeats = 1)
 #L4 <- biocorex(L2$labels, 1, 2, marginal_description = "discrete", repeats = 1)
 
@@ -26,7 +26,7 @@ p <- p + theme_void() + theme_transparent()
 
 #Generate sticker
 sticker(p, package="rcorex", p_size=12, p_y =1.1, p_color = "#000000",
-        s_x=1, s_y=1, s_width=1.6, s_height=1.4,
+        s_x=1, s_y=1, s_width=1.7, s_height=1.5,
         h_fill = "#FFFFFF", h_color = "#000000", h_size = 2,
         filename="man/figures/rcorexlogo.svg")
 
