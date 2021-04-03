b940884869ceebc2d0eec6ca92502bcb81cce161
jpkrooney <jrooney@rcsi.ie>
2021-04-04 01:24:17 +0200

    added bernoulli marginal test

--- tests/testthat/test-marginals_p_fns.R
+++ tests/testthat/test-marginals_p_fns.R
@@ -4,5 +4,6 @@ test_that("calculate_p_xi_given_y result checks", {
                   marginal_p_gaussian_result)
     expect_equal( marginal_p_discrete(x_i = x_i, thetai = theta_i_discrete_ns, dim_visible = 2),
                   marginal_p_discrete_result)
-
+    expect_equal( marginal_p_bernoulli(x_i = x_i, thetai = theta_i_bernoulli_ns),
+                  marginal_p_bernoulli_result)
 })
