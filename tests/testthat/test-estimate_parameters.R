6c39b2cdcba370080b5858325904f503919651b3
jpkrooney <jrooney@rcsi.ie>
2021-05-06 10:32:06 +0200

    added additional tests of estimate discrete and bernoulli parameters when n_hidden x dim_hidden produces an even number

--- tests/testthat/test-estimate_parameters.R
+++ tests/testthat/test-estimate_parameters.R
@@ -20,10 +20,42 @@ test_that("calculate_p_xi_given_y result checks", {
                                   p_y_given_x_3d = py_givenx_bern6x6,
                                   smooth_marginals = FALSE),
                  bern6result)
+
+
+
+    # check bernoulli and discrete algorithm when n_hidden x dim_hidden is an odd number
+    # for example 3x3 = 9, as certain potential speedups won't work in this case
+    expect_equal(estimate_parameters_bernoulli(x_i = databern6[, 1],
+                                    p_y_given_x_3d = array( c(py_givenx_bern6x6[ , , ]),
+                                                            dim = c(3,6,3)),
+                                    smooth_marginals = FALSE),
+                 structure(c(-0.00222643435128376, -6.10846692943418, -0.694316546828901,
+                             -0.691979180111471, -0.729418558613683, -0.658145496154548,
+                             -6.9343730193069, -0.000974207775732681, -0.694680828435557,
+                             -0.691615881158866, -0.642446382818512, -0.746556483689566,
+                             -7.5888323059681, -0.000506199743397317, -0.693120211700754,
+                             -0.693174150146476, -0.709390136288867, -0.677163847026783
+                 ), .Dim = c(2L, 3L, 3L)) )
+
+    expect_equal(estimate_parameters_discrete(x_i = databern6[, 1],
+                                    p_y_given_x_3d = array( c(py_givenx_bern6x6[ , , ]),
+                                                            dim = c(3,6,3)),
+                                    smooth_marginals = FALSE, dim_visible = 2),
+                 structure(c(-0.00222643435128376, -6.10846692943418, -0.694316546828901,
+                             -0.691979180111471, -0.729418558613683, -0.658145496154548,
+                             -6.9343730193069, -0.000974207775732681, -0.694680828435557,
+                             -0.691615881158866, -0.642446382818512, -0.746556483689566,
+                             -7.5888323059681, -0.000506199743397317, -0.693120211700754,
+                             -0.693174150146476, -0.709390136288867, -0.677163847026783
+                 ), .Dim = c(2L, 3L, 3L)) )
+
+
+
+
     # check discrete and bernoulli algorithm match on 6 by 6 data
     expect_equal(estimate_parameters_discrete(x_i = databern6[, 1],
-                                              p_y_given_x_3d = py_givenx_bern6x6,
-                                              smooth_marginals = FALSE, dim_visible = 2),
+                                    p_y_given_x_3d = py_givenx_bern6x6,
+                                    smooth_marginals = FALSE, dim_visible = 2),
                  estimate_parameters_bernoulli(x_i = databern6[, 1],
                                                p_y_given_x_3d = py_givenx_bern6x6,
                                                smooth_marginals = FALSE))
