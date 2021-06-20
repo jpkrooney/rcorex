f671f0826f3bf9f0c92e2c874cd305f4b1f0f3f5
jpkrooney <jrooney@rcsi.ie>
2021-06-20 13:11:24 +0200

    change start of negcheck to 10th iteration

--- R/epicorex.R
+++ R/epicorex.R
@@ -137,7 +137,7 @@ epicorex <- function(data, n_hidden = 1, dim_hidden = 2, marginal_description,
 
             # Detect persistent negative tcs and break out of loop if detected
             if ( nloop == negcheck_iter ){
-                if( all( sapply(tc_history, sum)[5:negcheck_iter] < neg_limit) ){
+                if( all( sapply(tc_history, sum)[10:negcheck_iter] < neg_limit) ){
                     state <- "Persistent negative tcs"
                     break
                 }
