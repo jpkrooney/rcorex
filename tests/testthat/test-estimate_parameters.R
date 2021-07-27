de7cf9087fe1848c29ccb3cda5c0a00d09804c84
jpkrooney <jrooney@rcsi.ie>
2021-07-27 23:26:46 +0100

    minor edits

--- tests/testthat/test-estimate_parameters.R
+++ tests/testthat/test-estimate_parameters.R
@@ -16,20 +16,20 @@ test_that("calculate_p_xi_given_y result checks", {
                                                 p_y_given_x_3d = p_y_given_x_3d_estimate_discrete,
                                                 smooth_marginals = FALSE))
     # check bernoulli algorithm on higher dimension data (6 by 6)
-    expect_equal(estimate_parameters_bernoulli(x_i = databern6[, 1],
+    expect_equal( estimate_parameters_bernoulli(x_i = databern6[, 1],
                                   p_y_given_x_3d = py_givenx_bern6x6,
                                   smooth_marginals = FALSE),
-                 bern6result)
+                  bern6result)
 
 
 
     # check bernoulli and discrete algorithm when n_hidden x dim_hidden is an odd number
     # for example 3x3 = 9, as certain potential speedups won't work in this case
-    expect_equal(estimate_parameters_bernoulli(x_i = databern6[, 1],
+    expect_equal( estimate_parameters_bernoulli(x_i = databern6[, 1],
                                     p_y_given_x_3d = array( c(py_givenx_bern6x6[ , , ]),
                                                             dim = c(3,6,3)),
                                     smooth_marginals = FALSE),
-                 structure(c(-0.00222643435128376, -6.10846692943418, -0.694316546828901,
+                  structure(c(-0.00222643435128376, -6.10846692943418, -0.694316546828901,
                              -0.691979180111471, -0.729418558613683, -0.658145496154548,
                              -6.9343730193069, -0.000974207775732681, -0.694680828435557,
                              -0.691615881158866, -0.642446382818512, -0.746556483689566,
