c1bf6ffe174ffb052801db120009fe3cf09ce76c
jpkrooney <jrooney@rcsi.ie>
2021-07-28 08:54:07 +0100

    added calculate_mis test

---
+++ tests/testthat/test-calculate_mis.R
@@ -0,0 +1,13 @@
+context("calculate_mis")
+test_that("calculate_mis function calculates expected result", {
+
+    # Run tests
+    expect_equal(with_seed( calculate_mis(data = data, theta = theta_result_gaussian_no_smooth,
+                                          marginal_description = "gaussian",
+                                          log_p_y = log_p_y, p_y_given_x_3d = p_y_given_x_3d,
+                                          dim_visible = NULL, logpx_method = "pycorex"), seed=555),
+                 structure(c(-0.0941962762876255, 0.00954192205367097, -0.0941962762876255,
+                             0.00954192205367097, -0.0941962762876255, 0.00954192205367097,
+                             -0.0995261041131583, -0.000778754396565922, -0.0995261041131583,
+                             -0.000778754396565922), .Dim = c(2L, 5L)))
+})
