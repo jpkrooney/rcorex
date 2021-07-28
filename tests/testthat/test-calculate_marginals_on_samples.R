d26512447c9d61a83cec5053b6cd4c8e85c4926b
jpkrooney <jrooney@rcsi.ie>
2021-07-28 15:31:32 +0100

    added tests

--- tests/testthat/test-calculate_marginals_on_samples.R
+++ tests/testthat/test-calculate_marginals_on_samples.R
@@ -1,4 +1,6 @@
 test_that("calculate_marginals_on_samples works correctly", {
+    source(system.file("testdata", "testdata_calculate_marginals_on_samples.R",
+                                 package="rcorex", mustWork=TRUE))
     expect_equal(calculate_marginals_on_samples(data = data,
                                                 theta = theta_result_gaussian_no_smooth,
                                                 marginal_description = "gaussian",
@@ -14,4 +16,20 @@ test_that("calculate_marginals_on_samples works correctly", {
                                                 dim_visible = dim_visible,
                                                 logpx_method = "pycorex"),
                  log_marg_x_4d_discrete)
+
+    expect_equal(calculate_marginals_on_samples(data = data,
+                                                theta = theta_result_gaussian_no_smooth,
+                                                marginal_description = "gaussian",
+                                                log_p_y = log_p_y,
+                                                dim_visible = NULL,
+                                                logpx_method = "mean"),
+                 result_log_marg_x_4d_gaussian_mean)
+
+    expect_equal(calculate_marginals_on_samples(data = data,
+                                                theta = theta_result_discrete_no_smooth,
+                                                marginal_description = "discrete",
+                                                log_p_y = log_p_y,
+                                                dim_visible = dim_visible,
+                                                logpx_method = "pycorex"),
+                 result_log_marg_x_4d_discrete_mean)
 })
