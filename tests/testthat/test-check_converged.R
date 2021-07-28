9c31cc38018ad4d28c51cf969e7bdd497cec90fa
jpkrooney <jrooney@rcsi.ie>
2021-07-28 13:59:32 +0100

    added test for check_converged

--- tests/testthat/test-check_converged.R
+++ tests/testthat/test-check_converged.R
@@ -1,4 +1,5 @@
 test_that("check_converged works", {
     expect_equal(check_converged(tc_hist_converged, eps=1e-5), TRUE)
     expect_equal(check_converged(tc_hist_converged, eps=1e-7), FALSE)
+    expect_equal(check_converged(tc_hist_converged[1:5], eps=1e-5), FALSE)
 })
